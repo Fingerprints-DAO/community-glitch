1a5896193edd711d4aa7c86719a4442980b0c67b
arod.studio <arod.mail@proton.me>
2024-04-14 12:51:02 -0300

    fix(versions): add an easier way to have mainnet version

--- packages/dapp-web-app/src/app/page.tsx
+++ packages/dapp-web-app/src/app/page.tsx
@@ -3,12 +3,21 @@
 import FullPageTemplate from 'components/Templates/FullPage'
 import ArtGrid from 'components/ArtGrid'
 import { AuctionProvider } from 'contexts/AuctionContext'
+// import StaticArtGrid from 'components/ArtGrid/Static'
+import dynamic from 'next/dynamic'
 
+const DynamicArtGrid = dynamic(() => import('components/ArtGrid/Static'), {
+  ssr: false,
+})
 export default function Home() {
   return (
     <FullPageTemplate>
       <AuctionProvider>
-        <ArtGrid />
+        {process.env.NEXT_PUBLIC_WEB3_NETWORK === 'mainnet' ? (
+          <DynamicArtGrid />
+        ) : (
+          <ArtGrid />
+        )}
       </AuctionProvider>
     </FullPageTemplate>
   )
