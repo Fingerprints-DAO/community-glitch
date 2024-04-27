9ae774bfc52d702962113f2b8fea06227899f632
arod.studio <arod.mail@proton.me>
2024-04-27 15:38:42 -0300

    chore(deploy): glitchy to sepolia

--- packages/dapp-contracts/script/Deploy.Glitchy.s.sol
+++ packages/dapp-contracts/script/Deploy.Glitchy.s.sol
@@ -24,13 +24,14 @@ contract DeployGlitchySepolia is BaseScript {
   function run() public returns (GlitchyGridGrid glitchy) {
     address deployer = broadcaster;
     string memory baseURIGlitchyGridGrid = 'https://glitch.mishaderidder.com/mint-edition/metadata/';
-    uint256 startTime = block.timestamp + (3600 * 1);
-    uint256 endTime = startTime + (3600 * 10);
+    uint256 startTime = block.timestamp + (0);
+    uint256 endTime = startTime + (3600 * 5);
 
     vm.startBroadcast();
     glitchy = _deployGlitchyGridGrid(deployer, baseURIGlitchyGridGrid);
     glitchy.setConfig(startTime, endTime);
     glitchy.setDiscountAllowlistRoot(0x4327a733a1f371691a5f256a666697c4c6c66f58ad97072995821e0fc35ced65);
+    glitchy.setFreeClaimAllowlistRoot(0x2e9682bdda23b84ccad52e90f7f60a2747d877961cefaf058657f0c58a7ccf74);
     vm.stopBroadcast();
   }
 }
@@ -45,6 +46,7 @@ contract DeployGlitchyMainnet is BaseScript {
     glitchy = _deployGlitchyGridGrid(deployer, baseURIGlitchyGridGrid);
     glitchy.setConfig(startTime, endTime);
     glitchy.setDiscountAllowlistRoot(0x4327a733a1f371691a5f256a666697c4c6c66f58ad97072995821e0fc35ced65);
+    glitchy.setFreeClaimAllowlistRoot(0x2e9682bdda23b84ccad52e90f7f60a2747d877961cefaf058657f0c58a7ccf74);
 
     vm.stopBroadcast();
   }
